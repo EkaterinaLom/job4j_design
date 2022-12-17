eb835e2444b8475b1a86cf4ce836d43c1c84bf75
EkaterinaLom <Lomakinaei@mail.ru>
2022-12-17 19:52:53 +1000

    0.4. BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -18,7 +18,7 @@ public class LogFilter {
     }
 
     public static void save(List<String> log, String file) {
-        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))) {
+        try (PrintWriter out = new PrintWriter(file)) {
             log.forEach(out::println);
         } catch (IOException e) {
             e.printStackTrace();
