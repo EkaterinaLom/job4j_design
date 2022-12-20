e79ecdf2dc7e0ad18348fdff50897f83ead0bb5e
EkaterinaLom <Lomakinaei@mail.ru>
2022-12-20 21:08:56 +1000

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+
+    public void unavailable(String sourse, String target) {
+        boolean work = true;
+        try (BufferedReader read = new BufferedReader(new FileReader(sourse));
+             PrintWriter writer = new PrintWriter(target)) {
+            while (read.ready()) {
+                String[] strings = read.readLine().split(" ");
+                if (work && (strings[0].contains("400") || strings[0].contains("500"))) {
+                    work = false;
+                    writer.printf("%s;", strings[1]);
+                } else if (!work && (strings[0].contains("200") || strings[0].contains("300"))) {
+                    work = true;
+                    writer.printf("%s;\n", strings[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("server.log", "target.csv");
+    }
+}
