4217db04769e046c56d733b3b538b7827d1b6cca
EkaterinaLom <Lomakinaei@mail.ru>
2022-12-26 23:12:13 +1000

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -10,13 +10,26 @@ import java.util.function.Predicate;
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        valid(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void valid(String[] args) {
+        if (args.length == 0 || args.length != 2) {
+            throw new IllegalArgumentException("Arguments are null or invalid. Usage CLI_ARGUMENTS_TO_YOUR_APPLICATION.");
+        }
+        if (!Files.isDirectory(Path.of(args[0]))) {
+            throw new IllegalArgumentException("Verify that the start folder is correct, check of args[0]");
+        }
+        if (!args[1].equals(".js")) {
+            throw new IllegalArgumentException("Extension not specified correctly, check of args[1]");
+        }
     }
 }
