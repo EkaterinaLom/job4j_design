15839e76ee6e3f6d66f01e6d2e90cd421deba9b5
EkaterinaLom <Lomakinaei@mail.ru>
2023-02-01 21:42:36 +1000

    0. Select [#6861]

---
+++ db/fauna.sql
@@ -0,0 +1,15 @@
+create table fauna (
+	id serial primary key,
+	name text,
+	avg_age int,
+	discovery_date date
+);
+
+insert into fauna (name, avg_age, discovery_date) values ('fish', 2117, '1800-11-04');
+insert into fauna (name, avg_age, discovery_date) values ('mammalia', 12705, '1758-10-04');
+insert into fauna (name, avg_age, discovery_date) values ('birds', 5445, '1530-10-04');
+
+select * from fauna where name like '%fish%'; 
+select * from fauna where avg_age >= 10000 AND avg_age <= 21000; 
+select * from fauna where discovery_date is null; 
+select * from fauna where discovery_date < '1950-01-01'; 
