844731fcd46ddbc22d00e58f815442cae0fa4881
EkaterinaLom <Lomakinaei@mail.ru>
2023-02-02 20:42:43 +1000

    1. Схема прав пользователи и роли. [#1093 #386819]

--- db/Item/create.sql
+++ db/Item/create.sql
@@ -1,6 +1,42 @@
-create table itemes (
+create table roles (
 	id serial primary key,
 	name text
+);
+
+create table rules (
+	id serial primary key,
+	name text
+);
+
+create table category (
+	id serial primary key,
+	name text
+);
+
+create table status (
+	id serial primary key,
+	name text
+);
+
+create table roles_rules (
+	id serial primary key,
+	name text,
+	roles_id int references roles(id),
+	rules_id int references rules(id)
+);
+
+create table users (
+	id serial primary key,
+	name text,
+	roles_id int references roles(id),
+);
+
+create table itemes (
+	id serial primary key,
+	name text,
+	users_id int references users(id),
+	category_id int references category(id),
+	status_id int references status(id)
 );
 
 create table coments (
@@ -15,39 +51,3 @@ create table attached (
 	itemes_id int references itemes(id)
 );
 
-create table users (
-	id serial primary key,
-	name text,
-	itemes_id int references itemes(id)
-);
-
-create table roles (
-	id serial primary key,
-	name text,
-	users_id int references users(id)
-);
-
-create table rules (
-	id serial primary key,
-	name text
-);
-
-create table roles_rules (
-	id serial primary key,
-	name text,
-	roles_id int references roles(id),
-	rules_id int references rules(id)
-);
-
-
-create table category (
-	id serial primary key,
-	name text,
-	itemes_id int references itemes(id)
-);
-
-create table status (
-	id serial primary key,
-	name text,
-	itemes_id int references itemes(id)
-);
--- db/Item/insert.sql
+++ db/Item/insert.sql
@@ -1,9 +1,9 @@
-insert into itemes(name) values ('1');
+insert into roles (name) values ('customer');
+insert into rules (name) values ('super urgent');
+insert into category (name) values ('first');
+insert into status (name) values ('in operation');
+insert into roles_rules (name, roles_id, rules_id) values ('cstandart', 1, 1);
+insert into users (name, roles_id) values ('Sidorov', 1);
+insert into itemes(name, users_id, category_id, status_id) values ('1S', 1, 1, 1);
 insert into coments(name, itemes_id) values ('urgent', 1);
 insert into attached(name, itemes_id) values ('scans', 1);
-insert into users (name, itemes_id) values ('Sidorov', 1);
-insert into roles (name, users_id) values ('customer', 1);
-insert into rules (name) values ('super urgent');
-insert into roles_rules (name, roles_id, rules_id) values ('cstandart', 1, 1);
-insert into category (name, itemes_id) values ('first', 1);
-insert into status (name, itemes_id) values ('in operation', 1);
