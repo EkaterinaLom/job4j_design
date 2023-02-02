38ad163d0ef0e17171ed19a829094086c626b0eb
EkaterinaLom <Lomakinaei@mail.ru>
2023-02-02 20:01:14 +1000

    0. Select [#6861]

--- db/fauna.sql
+++ db/fauna.sql
@@ -8,8 +8,11 @@ create table fauna (
 insert into fauna (name, avg_age, discovery_date) values ('fish', 2117, '1800-11-04');
 insert into fauna (name, avg_age, discovery_date) values ('mammalia', 12705, '1758-10-04');
 insert into fauna (name, avg_age, discovery_date) values ('birds', 5445, '1530-10-04');
+insert into fauna (name, avg_age, discovery_date) values ('insects', 150, '1960-10-04');
+insert into fauna (name, avg_age, discovery_date) values ('worms', 150, null);
 
 select * from fauna where name like '%fish%'; 
 select * from fauna where avg_age >= 10000 AND avg_age <= 21000; 
 select * from fauna where discovery_date is null; 
 select * from fauna where discovery_date < '1950-01-01'; 
+select * from fauna where discovery_date > '1950-01-01';
