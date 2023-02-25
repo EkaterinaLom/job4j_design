39fede11f0af627b2f70e4ff5c1950fecce015db
EkaterinaLom <Lomakinaei@mail.ru>
2023-02-26 00:30:29 +1000

    0. JDBC [#6863]

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -48,6 +48,6 @@ public class Config {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Config("app.properties"));
+        System.out.println(new Config("/home/kate/Downloads/projects/job4j_design/data/app.properties"));
     }
 }
